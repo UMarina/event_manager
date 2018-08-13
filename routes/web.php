abd9db487c7ff36a672bd35cef0357515c085d8a
UMarina <m.udovitskaya@gmail.com>
2018-08-13 11:30:51 +0300

    routing

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 Route::get('/', 'EventController@index')->name('events');
 Route::get('/{id}', 'EventController@showEvent')->name('events.show');
-Route::get('{event_id}/users/create/', 'UserController@createUser')->name('users.create');
+Route::get('/{event_id}/users/create/', 'UserController@createUser')->name('users.create');
 Route::post('/users', 'UserController@store')->name('users.store');
